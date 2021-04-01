2ed7c9eee138adc9b7771a38be9a01f2adbb7fe4
vililcheva <81766393+vililcheva@users.noreply.github.com>
2021-04-01 19:19:49 +0300

    Validate Room

--- Data/Entity/Room.cs
+++ Data/Entity/Room.cs
@@ -1,4 +1,4 @@
-﻿using Data.Enumeration;
+using Data.Enumeration;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -14,10 +14,31 @@ namespace Data.Entity
         }
 
         public int Id { get; set; }
+
+
+        [Required]
+        [Display(Name = "Room number")]
         public int Number { get; set; }
+
+
+        [Required]
+        [Display(Name = "Room capacity")]
         public int Capacity { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price (Adult)")]
         public decimal PriceAdult { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price (child)")]
         public decimal PriceChild { get; set; }
+
+
+        [Required]
         public int Type { get; set; }
         public bool IsFree { get; set; }
         public virtual ICollection<Reservation> Reservations { get; set; }
