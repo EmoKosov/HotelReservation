a66b33b07d22eb242e3d59abfbcd57d89aa728c0
vililcheva <81766393+vililcheva@users.noreply.github.com>
2021-04-01 19:14:53 +0300

    Validation Client

--- Data/Entity/Client.cs
+++ Data/Entity/Client.cs
@@ -1,4 +1,4 @@
-﻿using Data.Enumeration;
+using Data.Enumeration;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -16,14 +16,32 @@ namespace Data.Entity
 
         public int Id { get; set; }
 
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(40, ErrorMessage = "Name must be no longer than 40 characters")]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
 
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(40, ErrorMessage = "Name must be no longer than 40 characters")]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
 
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(10, ErrorMessage = "Phone number cannot be longer than 10 characters)")]
         public string TelephoneNumber { get; set; }
 
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required]
         public bool IsAdult { get; set; }
 
         public virtual ICollection<ClientReservation> Reservations { get; set; }
