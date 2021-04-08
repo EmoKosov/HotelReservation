51bb6c5ea143fe578d6d128f2191845dadb65083
vililcheva <81766393+vililcheva@users.noreply.github.com>
2021-04-08 20:16:57 +0300

    Validation

--- Web/Models/Clients/ClientsEditViewModel.cs
+++ Web/Models/Clients/ClientsEditViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using Data.Enumeration;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,11 +6,30 @@ namespace Web.Models.Clients
 {
     public class ClientsEditViewModel
     {
+        [HiddenInput]
         public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[a-zA-Zа-яА-Я]+$", ErrorMessage = "Моля, използвайте само букви")]
+        [StringLength(40, ErrorMessage = "Name must be no longer than 40 characters")]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[a-zA-Zа-яА-Я]+$", ErrorMessage = "Моля, използвайте само букви")]
+        [StringLength(40, ErrorMessage = "Name must be no longer than 40 characters")]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(10, ErrorMessage = "Телефона не може да бъде по-дълъг от 10 символа)")]
         public string TelephoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
         public bool IsAdult { get; set; }
 
     }
