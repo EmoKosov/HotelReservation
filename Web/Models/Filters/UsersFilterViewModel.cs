8283ea91f450e1a90e332a2cac93613e2185af89
vililcheva <81766393+vililcheva@users.noreply.github.com>
2021-04-08 20:19:26 +0300

    Update UsersFilterViewModel.cs

--- Web/Models/Filters/UsersFilterViewModel.cs
+++ Web/Models/Filters/UsersFilterViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Data.Enumeration;
@@ -11,11 +11,28 @@ namespace Web.Models.Reservations
 {
     public class UsersFilterViewModel
     {
+        [StringLength(20, ErrorMessage = "Потребителското име не може да бъде по-дълго от 20 символа")]
         public string Username { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
 
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[a-zA-Zа-яА-Я]+$", ErrorMessage = "Името може да съдържа само букви")]
+        [StringLength(40, ErrorMessage = "Името не може да бъде по-дълго от 40 символа")]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[a-zA-Zа-яА-Я]+$", ErrorMessage = "Презимето може да съдържа само букви")]
+        [StringLength(40, ErrorMessage = "Презимето не може да бъде по-дълго от 40 символа")]
+        [Display(Name = "Middle name")]
+        public string MiddleName { get; set; }
+
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[a-zA-Zа-яА-Я]+$", ErrorMessage = "Фамилията може да съдържа само букви")]
+        [StringLength(40, ErrorMessage = "Фамилията не може да бъде по-дълга от 40 символа")]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Text)]
+        public string Email { get; set; }
     }
 }
