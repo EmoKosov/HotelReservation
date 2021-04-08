6d382e33105c4eb18479854c2b00b1ed5fc6faa3
vililcheva <81766393+vililcheva@users.noreply.github.com>
2021-04-08 20:22:27 +0300

    Validate reservations

--- Web/Models/Reservations/ReservationsCreateViewModel.cs
+++ Web/Models/Reservations/ReservationsCreateViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Data.Enumeration;
@@ -11,15 +11,31 @@ namespace Web.Models.Reservations
 {
     public class ReservationsCreateViewModel
     {
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime DateOfAccommodation { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateOfExemption { get; set; } = DateTime.UtcNow;
+
+
         public bool IsBreakfastIncluded { get; set; }
         public bool IsAllInclusive { get; set; }
         public decimal OverallBill { get; set; }
+
+
+
         public int RoomId { get; set; }
         public int UserId { get; set; }
+
         public IEnumerable<SelectListItem> Rooms { get; set; }
+
         public IEnumerable<SelectListItem> Users { get; set; }
+
         public string Message { get; set; }
 
     }
