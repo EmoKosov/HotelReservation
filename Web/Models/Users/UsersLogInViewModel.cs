d6670284315852311a1c3b0ee49f33e8c0f91b30
EmoKosov <64019649+EmoKosov@users.noreply.github.com>
2021-04-09 18:21:26 +0300

    Update UsersLogInViewModel.cs

--- Web/Models/Users/UsersLogInViewModel.cs
+++ Web/Models/Users/UsersLogInViewModel.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using Data.Enumeration;
 
 namespace Web.Models.Users
 {
     public class UsersLogInViewModel
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
         public string Message { get; set; }
 
